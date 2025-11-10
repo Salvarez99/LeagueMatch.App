cc2ed376cf04cc3043752a930eee8fe0908193d8
Salvarez99 <stephenalvarez3@gmail.com>
2025-11-10 10:04:05 -0500

    Common Styles

--- Client/league-match-app/components/common/HostCard.jsx
+++ Client/league-match-app/components/common/HostCard.jsx
@@ -1,44 +1,11 @@
 import { Text, TouchableOpacity, View } from "react-native";
-import Screen from "../../utils/dimensions";
+import { styles } from "./Styles/HostCardStyle";
 
 export default function HostCard({ style, buttonStyle, textStyle }) {
   return (
-    <View
-      style={[
-        {
-          flex: 1,
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-        },
-        style,
-      ]}
-    >
-      <TouchableOpacity
-        style={[
-          {
-            backgroundColor: "#D9D9D9",
-            height: Screen.height * 0.12,
-            width: Screen.width * 0.94,
-            borderRadius: 15,
-            elevation: 5,
-            justifyContent: "center",
-            alignItems: "center",
-          },
-          buttonStyle,
-        ]}
-      >
-        <Text
-          style={[
-            {
-              fontSize: 14,
-              color: "#000",
-            },
-            textStyle,
-          ]}
-        >
-          Host Card
-        </Text>
+    <View style={[styles.containerStyle, style]}>
+      <TouchableOpacity style={[styles.hostCardButtonStyle, buttonStyle]}>
+        <Text style={[styles.text, textStyle]}>Host Card</Text>
       </TouchableOpacity>
     </View>
   );
---
+++ Client/league-match-app/components/common/Styles/HostCardStyle.js
@@ -0,0 +1,24 @@
+import { StyleSheet } from "react-native";
+import Screen from "../../utils/dimensions";
+
+export const styles = StyleSheet.create({
+  containerStyle: {
+    flex: 1,
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  hostCardButtonStyle: {
+    backgroundColor: "#D9D9D9",
+    height: Screen.height * 0.12,
+    width: Screen.width * 0.94,
+    borderRadius: 15,
+    elevation: 5,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 14,
+    color: "#000",
+  },
+});
