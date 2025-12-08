f2e3ffefdaa0311ef3b1006e0cd5b5c10a22bf9b
Salvarez99 <stephenalvarez3@gmail.com>
2025-12-08 17:02:44 -0500

    Link Client ghost functions

--- Client/types/ILobbyApiRequest.ts
+++ Client/types/ILobbyApiRequest.ts
@@ -27,3 +27,15 @@ export interface UpdateChampion {
 export interface Kick {
   uid: string;
 }
+
+export interface addGhost {
+  ghostId: string;
+  gameMap: string;
+  position?: string;
+  championId?: string;
+}
+export interface updateGhost {
+  ghostId: string;
+  position: string | null;
+  championId: string | null;
+}
