58fd8c7e9a6250e212f1bd410dffdf2faa9f5ada
Salvarez99 <stephenalvarez3@gmail.com>
2025-12-17 17:39:13 -0500

    Install express and types added express app entry point

--- Server/functions/src/index.ts
+++ Server/functions/src/index.ts
@@ -1,6 +1,12 @@
+import * as functions from "firebase-functions";
+import { app } from "./app";
+
 import { lobbyController } from "./controllers/lobbyController";
 import { userController } from "./controllers/userController";
 import { exportFunctions } from "./utils/exportFunctions";
+
+
+export const api = functions.https.onRequest(app);
 
 Object.assign(exports, exportFunctions(lobbyController, "lobby"));
 Object.assign(exports, exportFunctions(userController, "user"));
